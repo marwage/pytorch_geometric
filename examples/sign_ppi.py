7d41a597af89d9057059fc6a54c5493426fdac5e
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-06-01 11:59:34 +0200

    start over with sign ppi

--- examples/sign_ppi.py
+++ examples/sign_ppi.py
@@ -8,19 +8,15 @@ from torch.nn import Linear
 from torch_geometric.datasets import PPI
 import torch_geometric.transforms as T
 
-logging.basicConfig(filename='sign_reddit.log',level=logging.DEBUG)
+logging.basicConfig(filename='sign_ppi.log',level=logging.DEBUG)
 start = time.time()
 
 
 K = 3
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PPI')
 transform = T.Compose([T.NormalizeFeatures(), T.SIGN(K)])
-train_dataset = PPI(path, transform=transform, split='train')
-val_dataset = PPI(path, transform=transform, split='val')
-test_dataset = PPI(path, transform=transform, split='test')
-train_data = train_dataset[0]
-val_data = val_dataset[0]
-test_data = test_dataset[0]
+dataset = PPI(path, transform=transform)
+data = dataset[0]
 
 time_preprocessing_data = time.time() - start
 logging.info("preprocessing data took " + str(time_preprocessing_data))
@@ -32,11 +28,11 @@ class Net(torch.nn.Module):
         self.lins = torch.nn.ModuleList()
         num_hidden_channels = 512
         for _ in range(K + 1):
-            self.lins.append(Linear(train_dataset.num_node_features, num_hidden_channels))
-        self.lin = Linear((K + 1) * num_hidden_channels, train_dataset.num_classes)
+            self.lins.append(Linear(dataset.num_node_features, num_hidden_channels))
+        self.lin = Linear((K + 1) * num_hidden_channels, dataset.num_classes)
 
     def forward(self):
-        xs = [train_data.x] + [train_data[f'x{i}'] for i in range(1, K + 1)]
+        xs = [data.x] + [data[f'x{i}'] for i in range(1, K + 1)]
         for i, lin in enumerate(self.lins):
             out = F.dropout(F.relu(lin(xs[i])), p=0.5, training=self.training)
             xs[i] = out
@@ -48,10 +44,7 @@ class Net(torch.nn.Module):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 start_copying = time.time()
-model = Net().to(device)
-train_data = train_data.to(device)
-val_data = val_data.to(device)
-test_data = test_data.to(device)
+model, data = Net().to(device), data.to(device)
 time_copying = time.time() - start_copying
 logging.info("copying took " + str(time_copying))
 
@@ -61,7 +54,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 def train():
     model.train()
     optimizer.zero_grad()
-    F.nll_loss(model()[train_data], train_data.y).backward()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
     optimizer.step()
 
 
@@ -69,9 +62,9 @@ def train():
 def test():
     model.eval()
     logits, accs = model(), []
-    for data in [train_data, val_data, test_data]:
-        pred = logits[data].max(1)[1]
-        acc = pred.eq(data.y).sum().item() / data.sum().item()
+    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
         accs.append(acc)
     return accs
 
