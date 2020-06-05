c830de83d6539316bd7228b96192fa7c02a73160
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-06-05 09:24:18 +0200

    output of run in log file

--- examples/run.sh
+++ examples/run.sh
@@ -2,10 +2,10 @@
 
 NAME="cluster_gcn_reddit_checkpoint"
 
-git rev-parse HEAD > ${NAME}.out
+git rev-parse HEAD >> ${NAME}.log
 
 nvidia-smi dmon -s umt -o T -f ${NAME}.smi &
 
-python3 ${NAME}.py >> ${NAME}.out
+python3 ${NAME}.py >> ${NAME}.log
 
 pkill -P $$
